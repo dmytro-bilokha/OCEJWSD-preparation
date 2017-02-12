feb1548a31fee03c40f79aa70525175cf8dd2402
Dmytro Bilokha <dmytro@posteo.net>
2017-02-12 18:13:44 +0200

    Created simple servlet

---
+++ SimpleServletRest/src/main/java/bilokhado/simpleservletrest/RestServlet.java
@@ -0,0 +1,59 @@
+package bilokhado.simpleservletrest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet providing RESTful web service for the 'country' resource
+ */
+@WebServlet(urlPatterns = "/country", loadOnStartup = 1)
+public class RestServlet extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOG.info("Got get request");
+        PrintWriter out = resp.getWriter();
+        out.println("Hello, people!");
+    }
+
+    @Override
+    protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doHead(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doOptions(req, resp);
+    }
+
+    @Override
+    protected void doTrace(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doTrace(req, resp);
+    }
+
+}
