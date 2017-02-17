c7969c424d58c94a63dab37c4a896d58c41f0755
Dmytro Bilokha <dmytro@posteo.net>
2017-02-17 19:29:12 +0200

    Implemented GET with country code, improved error reporting

--- TomcatJerseyRest/src/main/java/bilokhado/tomcatjerseyrest/RestCountryService.java
+++ TomcatJerseyRest/src/main/java/bilokhado/tomcatjerseyrest/RestCountryService.java
@@ -1,6 +1,7 @@
 package bilokhado.tomcatjerseyrest;
 
 import bilokhado.tomcatjerseyrest.dao.CountryDao;
+import bilokhado.tomcatjerseyrest.domain.Country;
 import bilokhado.tomcatjerseyrest.domain.CountryList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,7 @@ import javax.servlet.ServletContext;
 import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -19,7 +21,7 @@ import java.sql.SQLException;
 /**
  * REST service for the country object
  */
-@Path("/country")
+@Path("country")
 public class RestCountryService {
 
     private static final Logger LOG = LoggerFactory.getLogger(RestCountryService.class);
@@ -45,8 +47,30 @@ public class RestCountryService {
             return Response.ok(new CountryList(countryDao.getCountries())).build();
         } catch (SQLException ex) {
             LOG.error("Failed to get country list from the DB", ex);
-            return Response.serverError().entity("Failed to get country list from the DB").build();
+            return Response.serverError()
+                    .type(MediaType.TEXT_PLAIN_TYPE)
+                    .entity("Failed to get country list from the DB").build();
         }
     }
 
+    @Path("{code: [a-zA-Z]{3}}")
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getCountry(@PathParam("code") String code) {
+        try {
+            Country country = countryDao.getCountry(code);
+            if (country == null)
+                return Response.status(Response.Status.NOT_FOUND)
+                        .type(MediaType.TEXT_PLAIN_TYPE)
+                        .entity("Not found country with code='" + code + '\'')
+                        .build();
+            return Response.ok(country).build();
+        } catch (SQLException ex) {
+            LOG.error("Failed to get country with code='{}' from the DB", code, ex);
+            return Response.serverError()
+                    .type(MediaType.TEXT_PLAIN_TYPE)
+                    .entity("Failed to get country with code='" + code + "' from the DB")
+                    .build();
+        }
+    }
 }
--- TomcatJerseyRest/src/main/java/bilokhado/tomcatjerseyrest/dao/CountryDao.java
+++ TomcatJerseyRest/src/main/java/bilokhado/tomcatjerseyrest/dao/CountryDao.java
@@ -24,15 +24,54 @@ public class CountryDao {
     public List<Country> getCountries() throws SQLException {
         List<Country> fetchedCountries = new ArrayList<>();
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement("SELECT Code, Name FROM Country");
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT Code, Name FROM Country ORDER BY Code, Name");
              ResultSet resultSet = statement.executeQuery();) {
             while (resultSet.next()) {
-                Country country = new Country();
-                country.setCode(resultSet.getString("Code"));
-                country.setName(resultSet.getString("Name"));
-                fetchedCountries.add(country);
+                fetchedCountries.add(mapCountry(resultSet));
             }
         }
         return fetchedCountries;
     }
+
+    public Country getCountry(String code) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        Country country;
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(
+                     "SELECT Code, Name FROM Country WHERE Code=?");
+             statement.setString(1, code);
+             resultSet = statement.executeQuery();
+             boolean hasResult = resultSet.first();
+             if (hasResult)
+                 country = mapCountry(resultSet);
+             else
+                 country = null;
+        } finally {
+            closeIfNotNull(resultSet, statement, connection);
+        }
+        return country;
+    }
+
+    private Country mapCountry(ResultSet resultSet) throws SQLException {
+        Country country = new Country();
+        country.setCode(resultSet.getString("Code"));
+        country.setName(resultSet.getString("Name"));
+        return country;
+    }
+
+    private void closeIfNotNull(AutoCloseable... resources) {
+        for (AutoCloseable resource : resources) {
+            try {
+                if (resource != null)
+                    resource.close();
+            } catch (Exception ex) {
+                //We are closing, so just ignore exceptions for now
+            }
+        }
+    }
+
 }
