3428e6a30976c94f2142f67826f1bfb13816a540
Dmytro Bilokha <dmytro@posteo.net>
2017-02-23 20:11:11 +0200

    Removed unused import

--- TomcatMetroSoap/src/main/java/bilokhado/tomcatmetrosoap/SoapCountryService.java
+++ TomcatMetroSoap/src/main/java/bilokhado/tomcatmetrosoap/SoapCountryService.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.naming.Context;
